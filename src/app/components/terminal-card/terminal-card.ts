7dcca5115d71f883e6200723f3f4d0825759afea
UBSITStaMariaCMiaKyla <20232386@s.ubaguio.edu>
2026-05-13 03:38:12 +0800

    not-found UI done

--- src/app/components/terminal-card/terminal-card.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-terminal-card',
-  imports: [],
-  templateUrl: './terminal-card.html',
-  styleUrl: './terminal-card.css',
-})
-export class TerminalCard {}
